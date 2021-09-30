c9055dbb7ab1d8cc51f88feab034b9d4d58109ff
Mike Purvis <mikedotexe@gmail.com>
2021-09-30 09:38:45 -0700

    frontend calls claim_reward_new_account and claim_reward, and grab the linkdrop account from the state

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "regenerator-runtime/runtime";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -6,7 +7,7 @@ import { mungeBlockchainCrossword, viewMethodOnContract } from './utils';
 import { generateSeedPhrase } from 'near-seed-phrase';
 
 async function initCrossword() {
-  const nearConfig = getConfig(process.env.NODE_ENV || 'testnet');
+  const nearConfig = getConfig(process.env.NEAR_ENV || 'testnet');
 
   let existingKey = localStorage.getItem('playerKeyPair');
 
@@ -18,29 +19,29 @@ async function initCrossword() {
 
   // Get crossword puzzle using view method
   const chainData = await viewMethodOnContract(nearConfig, 'get_unsolved_puzzles');
-
   let data;
 
   // There may not be any crossword puzzles to solve, check this.
-  if (chainData.length) {
+  if (chainData.puzzles.length) {
     // Save the crossword solution's public key
     // Again, assuming there's only one crossword puzzle.
-    localStorage.setItem('crosswordSolutionPublicKey', chainData[0]['solution_public_key']);
-    data = mungeBlockchainCrossword(chainData);
+    localStorage.setItem('crosswordSolutionPublicKey', chainData.puzzles[0]['solution_public_key']);
+    data = mungeBlockchainCrossword(chainData.puzzles);
   } else {
     console.log("Oof, there's no crossword to play right now, friend.");
   }
+  let creatorAccount = chainData.creator_account;
 
-
-  return { nearConfig, data };
+  return { nearConfig, data, creatorAccount };
 }
 
 window.nearInitPromise = initCrossword()
-  .then(({ nearConfig, data }) => {
+  .then(({ nearConfig, data, creatorAccount }) => {
     ReactDOM.render(
       <App
         nearConfig={nearConfig}
         data={data}
+        creatorAccount={creatorAccount}
       />,
       document.getElementById('root'));
   });
