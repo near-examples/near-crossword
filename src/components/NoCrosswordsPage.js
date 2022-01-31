d5af7f80635f62269381b593bc9f776564e4ea93
Andrii Svirskyi <andrii.s@4irelabs.com>
2022-01-31 16:52:36 +0200

    redesign

--- src/components/NoCrosswordsPage.js
+++ src/components/NoCrosswordsPage.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const NoCrosswordsPage = () => {
-    return (
-        <div className="container no-puzzles">
-            <div className="title">All puzzles have been solved</div>
-            <div className="error-msg">Sorry friend, no crossword puzzles available at this time.<br/>In the meantime, check out the links below. :)
-            </div>
-            <div className="content"><a href="https://examples.near.org?from=crossword" className="btn" target="_blank">NEAR Examples<br/>(for developers)</a></div>
-            <div className="content"><a href="https://awesomenear.com?from=crossword" className="btn" target="_blank">Awesome NEAR projects.<br/>(DeFi, NFTs, games, comics…)</a></div>
+  return (
+    <div className="container no-puzzles">
+      <div className="successful-page-title">All puzzles have been solved</div>
+
+      <div className="successful-text">
+        Sorry friend, no crossword puzzles available at this time.
+      </div>
+      <div className="successful-text">
+        In the meantime, check out the links below.
+      </div>
+      <div className="arrows" />
+      <div className="success-links">
+        <div className="success-link">
+          <div className="bridge-text">For Developers </div>
+          <a
+            href="https://examples.near.org?from=crossword"
+            className="near-link"
+            target="_blank"
+          >
+            NEAR Examples
+          </a>
         </div>
-    )
-}
+        <div className="success-link">
+          <div className="bridge-text">DeFi, NFTs, games, comics, etc. </div>
+          <a
+            href="https://awesomenear.com?from=crossword"
+            className="near-link"
+            target="_blank"
+          >
+            Awesome NEAR projects
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default NoCrosswordsPage;
--- src/components/WonPage.js
+++ src/components/WonPage.js
@@ -1,94 +1,135 @@
 import React from "react";
-import {motion} from 'framer-motion/dist/framer-motion';
+import { motion } from "framer-motion/dist/framer-motion";
 
 const WonPage = ({
-                     claimStatusClasses,
-                     claimError,
-                     needsNewAccount,
-                     setNeedsNewAccount,
-                     claimPrize,
-                     playerKeyPair,
-                     nearConfig
-                 }) => {
-
-    async function claimAccountType(e) {
-        if (e.target.value === 'create-account') {
-            setNeedsNewAccount(true);
-        } else {
-            setNeedsNewAccount(false);
-        }
-    }
-
-    return (
-        <div className="container">
-            <div className="title">You won!</div>
-            <div className="error-msg">You still need to claim your prize.</div>
-            <div className="content">
-                <form action="">
-                    <div id="claim-status" className={claimStatusClasses}><p>{claimError}</p></div>
-                    <div className="field-group">
-                        <label htmlFor="claim-memo" className="sr-only">Enter your winning memo:</label>
-                        <input type="text" id="claim-memo" name="claim-memo" placeholder="Enter your winning memo:"/>
-                    </div>
-                    <div className="field-group">
-                        <div className="radio-field">
-                            <input
-                                type="radio"
-                                id="have-account"
-                                name="account-funding-radio"
-                                value="have-account"
-                                checked={needsNewAccount === false}
-                                onChange={claimAccountType}
-                            />
-                            <label htmlFor="have-account">I have an account</label>
-                        </div>
-                        <div className="radio-field">
-                            <input
-                                type="radio"
-                                id="create-account"
-                                name="account-funding-radio"
-                                value="create-account"
-                                checked={needsNewAccount === true}
-                                onChange={claimAccountType}
-                            />
-                            <label htmlFor="create-account">I need to create an account</label>
-                        </div>
-                    </div>
-
-
-                    <motion.div
-                        id="seed-phrase-wrapper"
-                        className="field-group"
-                        animate={{
-                            opacity: needsNewAccount === true ? 1 : 0,
-                            transitionEnd: {
-                                display: needsNewAccount === true?"block":"none",
-                            },
-                        }}
-                        transition={{duration: 0.5}}
-
-                    >
-                        <h3>You need to write this down, friend.</h3>
-                        <p id="seed-phrase">{playerKeyPair.seedPhrase}</p>
-                        <p>After you submit and it succeeds, use this seed phrase at <a
-                            href={nearConfig.walletUrl}
-                            target="_blank">NEAR Wallet</a>
-                        </p>
-                    </motion.div>
-
-
-                    <div className="field-group">
-                        <label htmlFor="claim-account-id" className="sr-only">Enter account name</label>
-                        <input type="text" id="claim-account-id" name="claim-account-id"
-                               placeholder="Enter account name"/>
-                    </div>
-
-
-                    <button type="submit" id="claim-button" onClick={claimPrize}>Submit</button>
-                </form>
-            </div>
+  claimStatusClasses,
+  claimError,
+  needsNewAccount,
+  setNeedsNewAccount,
+  claimPrize,
+  playerKeyPair,
+  nearConfig,
+}) => {
+  const [inputMemo, setInputMemo] = useState("");
+  const [inputName, setInputName] = useState("");
+  const isButtonDisabled = !inputMemo || !inputName;
+  return (
+    <div className="win-page">
+      <div className="title">You won!</div>
+      <div className="error-wrap">
+        <div className="error-icon"></div>
+        <div className="win-page-error">
+          You still need to claim your prize.
         </div>
-    );
-}
+      </div>
+      <div className="content">
+        <form action="">
+          <div id="claim-status" className={claimStatusClasses}>
+            <p>{claimError}</p>
+          </div>
+          <div className="field-group">
+            <label htmlFor="claim-memo" className="sr-only">
+              Enter your winning memo:
+            </label>
+            <input
+              type="text"
+              id="claim-memo"
+              name="claim-memo"
+              value={inputMemo}
+              onChange={(e) => {
+                setInputMemo(e.target.value);
+              }}
+              placeholder="Enter your winning memo:"
+            />
+          </div>
+          <div className="field-group">
+            <div
+              className="radio-field"
+              onClick={() => {
+                setNeedsNewAccount(false);
+              }}
+            >
+              <div
+                className={`radio-button ${
+                  needsNewAccount === false && "active"
+                }`}
+              >
+                {needsNewAccount === false && (
+                  <div className="radio-button-content" />
+                )}
+              </div>
+              <div>I have an account</div>
+            </div>
+            <div className="radio-field">
+              <div
+                className="radio-field"
+                onClick={() => {
+                  setNeedsNewAccount(true);
+                }}
+              >
+                <div
+                  className={`radio-button ${
+                    needsNewAccount === true && "active"
+                  }`}
+                >
+                  {needsNewAccount === true && (
+                    <div className="radio-button-content" />
+                  )}
+                </div>
+                <div> I need to create an account</div>
+              </div>
+            </div>
+          </div>
+
+          <motion.div
+            id="seed-phrase-wrapper"
+            className="field-group"
+            animate={{
+              opacity: needsNewAccount === true ? 1 : 0,
+              transitionEnd: {
+                display: needsNewAccount === true ? "block" : "none",
+              },
+            }}
+            transition={{ duration: 0.5 }}
+          >
+            <h3>You need to write this down, friend.</h3>
+            <p id="seed-phrase">{playerKeyPair.seedPhrase}</p>
+            <p>
+              After you submit and it succeeds, use this seed phrase at{" "}
+              <a href={nearConfig.walletUrl} target="_blank">
+                NEAR Wallet
+              </a>
+            </p>
+          </motion.div>
+
+          <div className="field-group">
+            <label htmlFor="claim-account-id" className="sr-only">
+              Enter account name
+            </label>
+            <input
+              type="text"
+              id="claim-account-id"
+              name="claim-account-id"
+              placeholder="Enter account name"
+              value={inputName}
+              onChange={(e) => {
+                setInputName(e.target.value);
+              }}
+            />
+          </div>
+          <div className="button-wrap">
+            <button
+              type="submit"
+              className={`win-button ${isButtonDisabled ? "disabled" : ""}`}
+              onClick={claimPrize}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default WonPage;
